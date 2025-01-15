1581a4fd9eefa515b9c9c861033fbcfcb3cf7b2b
JarkkoKarki <karkijarkko@gmail.com>
2025-01-15 13:05:03 +0200

    1.3.1

---
+++ 1.3/src/QuadraticEquation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        int a = scanner.nextInt();
+        System.out.println("b: ");
+        int b = scanner.nextInt();
+        System.out.println("c: ");
+        int c = scanner.nextInt();
+    }
+}
